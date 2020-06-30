f987ba83b34df7fe5d668e2cfd49d15a00b8abc5
Everton <ton.oliveira@sempre.facig.edu.br>
2020-06-30 08:15:41 -0300

    Faz cadastr na aplicação

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,6 +14,8 @@ import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import api from '../../services/api'
+
 import getValidationErrors from '../../utils/getValidadtionErrors';
 
 import Input from '../../components/Input';
@@ -56,12 +58,13 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
-      // history.push('/')
+      await api.post('/users', data);
+
+      navigation.goBack()
 
       Alert.alert(
         'Cadastro realizado com sucesso!',
-        'Vocêjá pode realizar login na aplicação',
+        'Você já pode realizar login na aplicação',
       );
 
 
