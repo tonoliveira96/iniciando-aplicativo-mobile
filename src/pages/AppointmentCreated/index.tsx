03b221d33596ab6bf5d5cf143cbd09aeec6b3c98
Everton <ton.oliveira@sempre.facig.edu.br>
2020-08-18 22:29:22 -0300

    Formatando a data

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { format, Locale } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import { View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+interface RouterParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const { params } = useRoute();
+
+  const RouterParams = params as RouterParams;
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      RouterParams.date,
+      "EEEE ', dia ' dd 'de' MMM 'de' YYYY 'às' HH:mm'h'",
+      { locale: ptBR },
+    );
+  }, [RouterParams.date]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
